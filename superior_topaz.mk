9ba1114f215237114625737beae03a4eaf68c37b
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-06-27 14:54:12 +0330

    topaz: More detail in device name + bringup

--- superior_topaz.mk
+++ superior_topaz.mk
@@ -14,10 +14,10 @@ $(call inherit-product, device/xiaomi/topaz/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_topaz
+PRODUCT_NAME := superior_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 12 4G
+PRODUCT_MODEL := Redmi Note 12 4G NFC
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
