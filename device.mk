203c42f78c739f422c81cce8e0fcd8b11459116a
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-06-29 09:55:29 +0330

    topaz: Adding TARGET_BOOT_ANIMATION_RES Value

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_bengal_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_bengal_idp_india.xml
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
